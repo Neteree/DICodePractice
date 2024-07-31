c254c496d02c309bc8633dec6531b3d807d20811
Neteree <57681573+Neteree@users.noreply.github.com>
2024-07-31 21:58:49 +1200

    Uploaded 02-group-exercise-22t3-undefined-group-3-6

---
+++ 02-group-exercise-22t3-undefined-group-3-6/server/src/app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const chirpModel = require("../models/ChirpModel");
+const formatChirp = require("./formatChirp");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/chirps", async (request, response) => {
+  const chirps = await chirpModel.find({}).lean();
+  const formattedChirps = chirps.map((chirp) => {
+    return formatChirp(chirp);
+  });
+  response.send(formattedChirps);
+});
+
+app.get("/chirps/:id", async (request, response) => {
+  const { id } = request.params;
+  const chirp = await chirpModel.findById(id).lean();
+  const formattedChirp = formatChirp(chirp);
+
+  response.send(formattedChirp);
+});
+
+app.post("/chirps", async (request, response) => {
+  const body = {
+    ...request.body,
+
+    replies: 0,
+    rechirps: 0,
+    likes: 0,
+  };
+
+  switch (body.uniqueName) {
+    case "@Jeph":
+      body.userName = "Jeph";
+      body.avatar = "jeph.png";
+      break;
+  }
+
+  const chirp = await chirpModel.create(body);
+  console.log(body);
+  response.status(201).send(chirp);
+});
+
+// app.post("/chirps", (request, response) => {
+//   const chirp = {
+//     ...request.body,
+//     chirpId: chirps.length + 1,
+//     date: "2023-01-27T03:23:43.233Z",
+//     rechirps: 0,
+//     replies: 0,
+//   };
+
+//   if (chirp.uniqueName === "@Jeph") {
+//     chirp.userName = "Jeph";
+//     chirp.userImage = "jeph.gif";
+//   }
+
+//   chirps.push(chirp);
+//   response.status(201).send(chirp);
+// });
+
+module.exports = app;
